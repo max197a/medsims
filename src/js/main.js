0189ada72e527621a0ec2d6834e26bfd5bf985a9
max197a <max197aa@gmail.com>
2018-12-20 01:42:18 +0200

    scroll fixes

--- src/js/main.js
+++ src/js/main.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
     initSliders();
     // initParallax();
     initValidations();
-    // initSmoothScroll();
+    initSmoothScroll();
   }
 
   // this is a master function which should have all functionality
@@ -160,35 +160,35 @@ $(document).ready(function() {
     });
   }
 
-  // //////////
-  // // SLIDERS
-  // //////////
+  //////////
+  // SLIDERS
+  //////////
 
-  // function initSmoothScroll() {
-  //   $(function() {
-  //     var $window = $(window); //Window object
+  function initSmoothScroll() {
+    $(function() {
+      var $window = $(window); //Window object
 
-  //     var scrollTime = 1.2; //Scroll time
-  //     var scrollDistance = 170; //Distance. Use smaller value for shorter scroll and greater value for longer scroll
+      var scrollTime = 1.2; //Scroll time
+      var scrollDistance = 170; //Distance. Use smaller value for shorter scroll and greater value for longer scroll
 
-  //     $window.on("mousewheel DOMMouseScroll", function(event) {
-  //       event.preventDefault();
+      $window.on("mousewheel DOMMouseScroll", function(event) {
+        event.preventDefault();
 
-  //       var delta =
-  //         event.originalEvent.wheelDelta / 120 ||
-  //         -event.originalEvent.detail / 3;
-  //       var scrollTop = $window.scrollTop();
-  //       var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+        var delta =
+          event.originalEvent.wheelDelta / 120 ||
+          -event.originalEvent.detail / 3;
+        var scrollTop = $window.scrollTop();
+        var finalScroll = scrollTop - parseInt(delta * scrollDistance);
 
-  //       TweenMax.to($window, scrollTime, {
-  //         scrollTo: { y: finalScroll, autoKill: true },
-  //         ease: Power1.easeOut, //For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
-  //         autoKill: true,
-  //         overwrite: 5
-  //       });
-  //     });
-  //   });
-  // }
+        TweenMax.to($window, scrollTime, {
+          scrollTo: { y: finalScroll, autoKill: true },
+          ease: Power1.easeOut, //For more easing functions see https://api.greensock.com/js/com/greensock/easing/package-detail.html
+          autoKill: true,
+          overwrite: 5
+        });
+      });
+    });
+  }
 
   //////////
   // SLIDERS
